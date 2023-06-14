89607b963260fe3be4730112e5ea96be2334ed1c
chabitblunk <cblunk097@gmail.com>
2023-06-14 12:14:26 -0600

    Split code into lib crate

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use minigrep::Config;
+use minigrep::run;
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
 
 fn main() {
@@ -19,27 +19,3 @@ fn main() {
     }
 }
 
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-    println!("{}", contents);
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        // make sure we have the proper amount of args
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        Ok(Config { query, file_path })
-    }
-}
