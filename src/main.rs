fca654f1e0e3a0c63615a4d5385f418ce4fbc540
chabitblunk <cblunk097@gmail.com>
2023-06-14 11:52:49 -0600

    Add impl Config block

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,21 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
-    let query = &args[1];
-    let file_path = &args[2];
-    println!("Searching for \"{}\" in file {}", query, file_path);
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read file");
+    let config = Config::new(&args);
+    println!("Searching for \"{}\" in file {}", config.query, config.file_path);
+    let contents = fs::read_to_string(config.file_path).expect("Should have been able to read file");
     println!("{}", contents);
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Config { query, file_path }
+    }
 }
